a2d14258118b03a3d526ea89b4a2cf13c06f0918
shayanjunaidtalat <shayanjunaidtalat@gmail.com>
2026-02-22 19:39:25 +0100

    new changes. Mawaqit is updated daily

--- modules/MMM-Mawaqit/MMM-Mawaqit.js
+++ modules/MMM-Mawaqit/MMM-Mawaqit.js
@@ -5,6 +5,19 @@ Module.register("MMM-Mawaqit", {
 
   start() {
     Log.info("Starting module: " + this.name);
+    this.scheduleNextUpdate();
+  },
+
+  scheduleNextUpdate() {
+    const now = new Date();
+    const tomorrow = new Date();
+    tomorrow.setHours(24, 0, 0, 0); // next midnight
+    const delay = tomorrow - now;
+
+    setTimeout(() => {
+      this.updateDom(0); // refresh iframe
+      this.scheduleNextUpdate(); // schedule next refresh
+    }, delay);
   },
 
   getDom() {
